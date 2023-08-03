b87c01d98b327f2e66aff8af5ea39a3b8b4e619a
Ian Hollands (C) <ihollands@eitccorp.com>
2023-08-03 12:58:33 -0400

    manually adding opengraph meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,36 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         />
+
+        <meta
+          property="og:url"
+          content="https://www.flyweight.xyz/"
+        />
+
+        <meta
+          property="og:title"
+          content="Flyweight: Brand Design"
+        />
+
+        <meta
+          property="og:description"
+          content="Leaner and meaner brand design. Heavy hitting creative power meets a nimble project approach."
+        />
+
+        <meta
+          property="og:image"
+          content="https://www.flyweight.xyz/opengraph-image.png"
+        />
+
+        <meta
+          property="og:site_name"
+          content="Flyweight"
+        />
+
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+        />
       </Head>
       <body>
         <Main />
